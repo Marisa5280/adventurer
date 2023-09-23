e5fdccd68a08e0f4f2dcee5b02684898914fb501
Marisa Wyatt <risawyatt22@gmail.com>
2023-09-23 13:48:43 -0600

    Add propTypes to Race

--- src/Components/Race/Race.js
+++ src/Components/Race/Race.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import PropTypes from 'prop-types';
 import { getPromise } from "../../apiCalls";
 
 function Race({ race, setRace, endpoint }) {
@@ -26,3 +27,26 @@ function Race({ race, setRace, endpoint }) {
 }
 
 export default Race;
+
+Race.propTypes = {
+  abilities: PropTypes.arrayOf(PropTypes.object),
+  page: PropTypes.shape({
+    "index": PropTypes.string,
+    "name": PropTypes.string,
+    "speed": PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    "ability_bonuses": PropTypes.arrayOf(PropTypes.object),
+    "ability_bonus_options": PropTypes.object,
+    "alignment":PropTypes.string,
+    "age": PropTypes.string,
+    "size": PropTypes.string,
+    "size_description": PropTypes.string,
+    "starting_proficiencies": PropTypes.array,
+    "starting_proficiency_options": PropTypes.object,
+    "languages": PropTypes.arrayOf(PropTypes.object),
+    "language_options": PropTypes.object,
+    "language_desc": PropTypes.string,
+    "traits": PropTypes.arrayOf(PropTypes.object),
+    "subraces": PropTypes.array,
+    "url": PropTypes.string
+})
+}
